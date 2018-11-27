4b96acfa6b895863264a586fea144e2062d2e598
Simon <13297255524@163.com>
2018-11-27 16:40:18 +0800

    simon-home first commit

---
+++ simon-home/src/main/java/com/my/room/simonhome/SimonHomeApplication.java
@@ -0,0 +1,12 @@
+package com.my.room.simonhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimonHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimonHomeApplication.class, args);
+    }
+}
